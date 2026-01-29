561cbce2db60255fbe7b457d7b576c91a37361fe
dallas-cyber <dallas@wongforcongress.com>
2026-01-29 15:52:26 -0700

    Create rss-wall.js

---
+++ rss-wall.js
@@ -0,0 +1,13 @@
+(function () {
+  function loadWall() {
+    var wall = document.createElement("rssapp-wall");
+    wall.setAttribute("id", "_Csr2yTzt0cnf3Q7C");
+    document.body.appendChild(wall);
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", loadWall);
+  } else {
+    loadWall();
+  }
+})();
